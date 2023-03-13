a72a7817fdb76671ecc1517869e42488933b2f5c
chedieck <estevao@lobo.dev.br>
2023-03-14 01:13:20 +0300

    fix: line break on header

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn get_main_csv_path() -> Result<path::PathBuf, Box<dyn Error>>{
         let dir_path = get_or_crate_data_dir()?;
         let file_path = dir_path.join("main.csv");
         if !file_path.is_file() {
-            let header_string = "search_term|search_result|title|main_translation|other_translations|overview|context_phrase";
+            let header_string = "search_term|search_result|title|main_translation|other_translations|overview|context_phrase\n";
             let mut file = std::fs::File::create(&file_path)?;
             file.write_all(header_string.as_bytes())?;
         }
